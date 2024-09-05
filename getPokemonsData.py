d3e1472bbf5eaf00bf864a92860de1d14c9ffd10
DaronV2 <darkenciel31@gmail.com>
2024-09-05 21:16:26 +0200

    Commencement du webscrap des données des pokémons en python

--- getPokemonsData.py
+++ getPokemonsData.py
@@ -1,9 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+import re 
+import json
 
-url = "https://www.pokepedia.fr/Liste_des_Pokémon_dans_l%27ordre_du_Pokédex_National#Liste_des_Pokémon"
+class Pokemon(object):
+    def __init__(self, poke_number):
+        self.poke_number = poke_number
+
+url = "https://pokemondb.net/pokedex/all"
 
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html')
 
-print(soup.find_all("table"))
+content = BeautifulSoup(
+    "\n".join(map(str, soup.select("#pokedex"))), "html.parser"
+)
+
+def get_pokemon_datas(content,contenttext):
+    # for i in content:
+    poke_number = re.findall(f"data-sort-value=\"\d+\"",content)
+    poke_numbers_list = re.findall(f"\d+",str(poke_number))
+    poke_name = re.findall(f"href=\"/pokedex/") #TODO
+    print(contenttext)
+
+
+get_pokemon_datas(str(content),content.text)
+
+
+# for i in content.children:
+#     print(i)
+
+
+    
+    
+
