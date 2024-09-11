47b7af6b7aea0f12c5c5f814772c3100d8364182
DaronV2 <darkenciel31@gmail.com>
2024-09-11 21:35:56 +0200

    Ajout du loader, clean code et fix fes types pokemons

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 class Pokemon {
-    constructor(NameStr, NumberInt, SpriteStr){
+    constructor(NameStr, NumberInt, SpriteStr, TypesPokemonStr){
         this.NameStr = NameStr;
         this.NumberInt = NumberInt; 
         this.SpriteStr = SpriteStr;
+        this.TypesPokemonStr = TypesPokemonStr;
     }
 }
 
 let table = [];
-let numbers = ["001","002"];
 
-// fetch('https://tyradex.vercel.app/api/v1/pokemon')
-//     .then((response) => response.json())
-//     .then((json) => parseJson(json))
-
-
-// function parseJson(json){
-    // let user = JSON.parse(json);
-    // console.log(user.pokedex_id);
-// }
+const startTime = performance.now();
 async function fetchJsonData() {
     try {
         const response = await fetch('https://tyradex.vercel.app/api/v1/pokemon');
@@ -26,57 +18,50 @@ async function fetchJsonData() {
             throw new Error('Erreur réseau : ' + response.statusText);
         }
 
-        const jsonData = await response.json(); // Désérialise et assigne à la variable
-        //console.log(jsonData); // Utilise la variable jsonData comme nécessaire
-
+        const jsonData = await response.json(); 
+       
         return jsonData; // Retourne les données si besoin d'utilisation externe
     } catch (error) {
         console.error('Erreur lors de la récupération du fichier JSON :', error);
     }
 }
 
-fetchJsonData().then(data => {
+async function main(){
+
+    const data = await fetchJsonData();
+    
     if (data){
-        data.forEach(element => {
-            let pokeNumber = element.pokedex_id;
-            let pokeName = element.name.fr;
-            let pokeSprite = element.sprites.regular;
-            const poke = new Pokemon(pokeName,pokeNumber,pokeSprite);
-            table.push(poke);
+        const spritePromises = data.map(async (element) => {
+            const pokeNumber = element.pokedex_id;
+            const pokeName = element.name.fr;
+            const pokeSprite = await getSprites(pokeNumber); 
+            let pokeTypes = [];
+            const types = element.types;
+            for (const type in types){
+                pokeTypes.push(types[type]);
+            }
+            return new Pokemon(pokeName, pokeNumber, pokeSprite, pokeTypes);
         });
+
+        // Attendre que toutes les promesses soient résolues
+        const pokemonList = await Promise.all(spritePromises);
+
+        // Ajouter tous les Pokémon dans le tableau
+        table.push(...pokemonList);
+
         for ( let j = 1 ; j < table.length ; j++){
-            console.log(table[j]);
-            pokemonObj = table[j];
-            createDivPoke(pokemonObj.NameStr + " #" + addZeros(pokemonObj.NumberInt) + pokemonObj.NumberInt, pokemonObj.SpriteStr);
+             pokemonObj = table[j];
+            // createDivPoke(pokemonNameStr(pokemonObj), pokemonObj.SpriteStr);
+            createDivPoke(pokemonObj);
         }
     }else{
         console.log("pas de datas");
     }
-});
-
-
-
-// for (let i = 0; i < table.length; i++){
-//     let pokemon = document.createElement('div');
-//     let header = document.createElement('div');
-//     let closeBtn = document.createElement('div');
-//     closeBtn.className = "closeBtn";
-//     closeBtn.id = table[i];
-//     header.className = "headPoke";
-//     pokemon.className = "pokemon";
-//     pokemon.id = table[i];
-//     let pokemonName = document.createTextNode(table[i]);
-//     let X = document.createTextNode("X");
-//     pokemonName.className = "pokename";
-//     let test = document.getElementById("mainclasspokemons");
-//     pokemon.appendChild(header);
-//     header.appendChild(pokemonName);
-//     header.appendChild(closeBtn);
-//     closeBtn.appendChild(X);
-//     test.appendChild(pokemon);
-//     //elmnt.className("pokemon");
-//     closeBtn.addEventListener("click", () => removeDiv(pokemon));
-// }
+    document.getElementById("loader-bg").style.display = 'none';
+    document.getElementById("loader").style.display = 'none';
+    const endTime = performance.now();  // Capture le temps de fin
+    console.log(`Execution Time: ${endTime - startTime} milliseconds`);  // Affiche la durée
+}
 
 function removeDiv(div){
     div.remove();
@@ -86,18 +71,22 @@ function readJSON(data){
     console.log(data);
 }
 
-function createDivPoke(pokeName,spriteStr){
+function pokemonNameStr(pokeObj){
+    return pokeObj.NameStr + " #" + addZeros(pokeObj.NumberInt) + pokeObj.NumberInt;
+}
+
+function createDivPoke(pokeObj){
     let pokemon = document.createElement('div');
     let header = document.createElement('div');
     let closeBtn = document.createElement('div');
     let sprite = document.createElement('img');
-    sprite.src = spriteStr;
+    sprite.src = pokeObj.SpriteStr;
     closeBtn.className = "closeBtn";
-    closeBtn.id = pokeName;
+    closeBtn.id = pokeObj.NameStr;
     header.className = "headPoke";
     pokemon.className = "pokemon";
-    pokemon.id = pokeName;
-    let pokemonName = document.createTextNode(pokeName);
+    pokemon.id = pokeObj.NameStr;//
+    let pokemonName = document.createTextNode(pokemonNameStr(pokemonObj));
     let X = document.createTextNode("X");
     pokemonName.className = "pokename";
     let test = document.getElementById("mainclasspokemons");
@@ -107,6 +96,10 @@ function createDivPoke(pokeName,spriteStr){
     header.appendChild(closeBtn);
     closeBtn.appendChild(X);
     test.appendChild(pokemon);
+    for (let types in pokeObj.TypesPokemonStr){
+        let type = document.createTextNode(pokeObj.TypesPokemonStr[types]);
+        pokemon.appendChild(type);
+    }
     closeBtn.addEventListener("click", () => removeDiv(pokemon));
 }
 
@@ -121,3 +114,24 @@ function addZeros(integer){
         return "";
     }
 }
+
+async function getSprites(pokeNumber){
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/'+pokeNumber, { cache: 'no-store' });
+        if (!response.ok){
+            throw new Error('Toujours pas');
+        }
+       
+        const url = await response.json();
+        const jsonData = url.sprites.front_default;
+         // Désérialise et assigne à la variable
+        //console.log(jsonData); // Utilise la variable jsonData comme nécessaire
+        
+        return jsonData; // Retourne les données si besoin d'utilisation externe
+    } catch (error) {
+        console.error('Erreur lors de la récupération du fichier JSON :', error);
+    }
+}
+
+
+main();
