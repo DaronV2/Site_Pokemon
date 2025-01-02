13958e2a402fb06299a3331506ea02ddbacb3c5f
DaronV2 <darkenciel31@gmail.com>
2025-01-02 15:31:09 +0100

    🔨 Add types colors to tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,26 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {},
+    colors: {
+        "plante": "#259e32",
+        "poison": "#72259e",
+        "feu": "#de1b27",
+        "vol": "#258a9e",
+        "eau": "#2e259e",
+        "insecte": "#4b9e25",
+        "normal": "#bfbfbe",
+        "electrik": "#e4ef3e",
+        "sol": "#828728",
+        "fee": "#d935e0",
+        "combat": "#ad4716",
+        "psy": "#a116ad",
+        "roche": "#c75923",
+        "acier": "#737373",
+        "glace": "#25c4d4",
+        "spectre": "#562f56",
+        "dragon": "#be8c25",
+        "tenebres": "#070548"
+    }    
   },
   plugins: [],
 }
